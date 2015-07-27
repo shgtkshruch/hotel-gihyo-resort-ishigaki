5895ce0aae6a59fadca6eda7da45a232753b3c24
shgtkshruch <shgtk.shruch@gmail.com>
2015-07-27 11:24:21 +0900

    Modified banner link address

--- src/front-page.php
+++ src/front-page.php
@@ -6,8 +6,8 @@
         <div class="aboutBlock block">
             <div class="banners">
                 <ul>
-                    <li><a href="about.html"><img src="<?php echo get_template_directory_uri(); ?>/images/home/bnr_about.png" height="97" width="320" alt="ホテル紹介"></a></li>
-                    <li><a href="access.html"><img src="<?php echo get_template_directory_uri(); ?>/images/home/bnr_access.png" height="97" width="320" alt="アクセス"></a></li>
+                    <li><a href="<?php echo get_permalink(25); ?>"><img src="<?php echo get_template_directory_uri(); ?>/images/home/bnr_about.png" height="97" width="320" alt="ホテル紹介"></a></li>
+                    <li><a href="<?php echo get_permalink(33); ?>"><img src="<?php echo get_template_directory_uri(); ?>/images/home/bnr_access.png" height="97" width="320" alt="アクセス"></a></li>
                 </ul>
             </div>
         </div><!-- /.aboutBlock -->
--- src/sidebar.php
+++ src/sidebar.php
@@ -1,5 +1,5 @@
 <p class="bnr">
-    <a href="contact.html"><img src="<?php echo get_template_directory_uri(); ?>/images/common/bnr_contact01.png" height="183" width="300" alt="宿泊のお問い合わせ"></a>
+    <a href="<?php echo get_permalink(29); ?>"><img src="<?php echo get_template_directory_uri(); ?>/images/common/bnr_contact01.png" height="183" width="300" alt="宿泊のお問い合わせ"></a>
 </p>
 <div class="wrapper">
     <section class="socialBanners">
@@ -8,7 +8,7 @@
             <ul>
                 <li><a href="" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/images/common/bnr_fb01.png" height="60" width="290" alt=""></a></li>
                 <li><a href="" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/images/common/bnr_tw01.png" height="60" width="290" alt=""></a></li>
-                <li><a href="" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/images/common/bnr_rss01.png" height="60" width="290" alt=""></a></li>
+                <li><a href="<?php echo home_url(); ?>/feed/" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/images/common/bnr_rss01.png" height="60" width="290" alt=""></a></li>
             </ul>
         </div>
     </section><!-- /.socialBanners -->
