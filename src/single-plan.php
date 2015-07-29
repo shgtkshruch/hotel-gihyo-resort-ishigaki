2b8cdb1313df8fd60050696fdb9af8e83c90f218
shgtkshruch <shgtk.shruch@gmail.com>
2015-07-29 15:18:54 +0900

    Add single-plan.php

---
+++ src/single-plan.php
@@ -0,0 +1,76 @@
+<?php get_header(); ?>
+
+    <div class="contentsWrap">
+        <div class="mainContents">
+            <?php
+            if ( have_posts() ) :
+              while ( have_posts() ) : the_post();
+            ?>
+            <article class="page">
+                <h1 class="type-A">宿泊プラン</h1>
+                <h2 class="title type-B"><span><?php the_title(); ?></span></h2>
+                <div class="plan">
+                    <div class="left">
+                    <?php
+                    $image = get_field('picture');
+                    $url = $image['sizes']['medium'];
+                    $width = $image['sizes']['medium-width'];
+                    $height = $image['sizes']['medium-height'];
+                    ?>
+                      <img src="<?php echo $url; ?>" width="<?php echo $width; ?>" height="<?php echo $height; ?>">
+                    </div>
+                    <div class="right">
+                        <?php the_content(); ?>
+                    </div>
+                </div>
+                <div class="plan">
+                    <div class="left">
+                        <dl>
+                            <dt>価格</dt>
+                            <dd><em><?php echo number_format(get_field('price')); ?>円／泊</em></dd>
+                        </dl>
+                        <dl>
+                            <dt>ベッド</dt>
+                            <dd>
+                            <?php
+                            $bed = get_field('bed');
+                            foreach ($bed as $key => $value) {
+                              echo $value;
+                              if ( $value !== end($bed) ) {
+                                echo '<br>';
+                              }
+                            }
+                            ?>
+                            </dd>
+                        </dl>
+                    </div>
+                    <div class="right">
+                        <dl>
+                            <dt>チェックイン</dt>
+                            <dd><?php echo nl2br(get_field('checkin')); ?></dd>
+                        </dl>
+                        <dl>
+                            <dt>朝食</dt>
+                            <dd>
+                            <?php if ( get_field('food') ) : ?>
+                            あり
+                            <?php else: ?>
+                            なし
+                            <?php endif;?>
+                            </dd>
+                        </dl>
+                    </div>
+                </div>
+            </article><!-- /.page -->
+        </div><!-- /.mainContents -->
+        <?php
+            endwhile;
+        endif;
+        ?>
+
+        <aside class="subContents">
+            <?php get_sidebar(); ?>
+        </aside><!-- /.subContents -->
+    </div><!-- /.contentsWrap -->
+
+<?php get_footer(); ?>
